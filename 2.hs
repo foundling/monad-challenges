5ce1c4c520d9fc480b801a9dcff050432be116e8
Alex Ramsdell <alexramsdell@gmail.com>
2018-09-10 18:57:37 -0700

    complete 2.2

--- 2.hs
+++ 2.hs
@@ -4,25 +4,42 @@
 module Set1 where
 import MCPrelude
 
-data Maybe a = Nothing | Just a 
+data Maybe a = Just a | Nothing
 
 instance Show a => Show (Maybe a) where
-    show Nothing = "Nothing"
-    show (Just a) = "Just " ++ show a
+  show (Just a) = "Just " ++ show a
+  show Nothing = "Nothing"
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:xs) = Just x 
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (x:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a,b)] -> Maybe b
+lookupMay k [] = Nothing
+lookupMay k (p:ps) = if (fst p) == k then Just (snd p) else lookupMay k ps 
+
+divMay :: (Eq a, Integral a) => a -> a -> Maybe a
+divMay x 0 = Nothing 
+divMay x y = Just $ div x y
 
 --
---data Person = String
+--    • In the expression: maximumMay y : xs
 --
---instance Show Person => Show Person where 
---    show Person = show 
+--Couldn't match expected type ‘Maybe a’
+--                  with actual type ‘[Maybe a0]’
 --
---
+maximumMay :: (Ord a) => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay [x] = Just x
+maximumMay [x,y] = Just $ min x y
+maximumMay (x:y:xs) = if x >= y then maximumMay (x:xs) else maximumMay (y:xs)
 
-data Expr = Val Int | Div Expr Expr
-
---eval :: Expr -> Int
---eval (Val a) = a
---eval safeDiv a b = div $ eval a $ eval b
-
-safeDiv (Val m) (Val 0) = Nothing
-safeDiv (Val m) (Val n) = Just $ div m n
+minimumMay :: (Ord a) => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay [x] = Just x
+minimumMay [x,y] = Just $ min x y
+minimumMay (x:y:xs) = if x <= y then minimumMay (x:xs) else minimumMay (y:xs) 
